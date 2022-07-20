4a8572a4685e0070230ea0a76c7974d10e5661c8
Ville Koskela <vkoskela@gmail.com>
2022-07-20 14:16:57 +0300

    about to be ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const updateUsername = (event: React.FormEvent<HTMLInputElement>) => {
+    setUsername(event.currentTarget.value);
+  };
+
+  const updatePassword = (event: React.FormEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const printToConsole = () => {
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={username} onChange={updateUsername} /> <br />
+      <input type="password" value={password} onChange={updatePassword} /> <br />
+      <button onClick={printToConsole}>login</button>
     </div>
   );
 }
